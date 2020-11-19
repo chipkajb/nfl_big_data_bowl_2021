24828780190f67914543b9705cc762259625e7fb
Jordan Chipka <jbc274@cornell.edu>
2020-11-18 19:41:27 -0500

    added initial version of play animator

--- scripts/visualize_data.py
+++ scripts/visualize_data.py
@@ -1,6 +1,7 @@
 import numpy as np
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+from pdb import set_trace as bp
+#import warnings
+#warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # append parent directory to Python path
 import sys
@@ -15,19 +16,35 @@ from util.visualization import *
 week = 1
 week_df = load_week_df(week)
 
-# get play data frame
+# get play and game IDs
 play_idx = 0
 play_id_list = np.unique(week_df["playId"])
 play_df = get_play_df(week_df, play_id_list, play_idx)
 
-# define offensive/defensive positions
-defense_positions = np.array(['CB','DB','DE','DL','FS','ILB','LB','MLB','NT','OLB','S','SS'])
-offense_positions = np.array(['FB','HB','QB','RB','TE','WR'])
+## define offensive/defensive positions
+#defense_positions = np.array(['CB','DB','DE','DL','FS','ILB','LB','MLB','NT','OLB','S','SS'])
+#offense_positions = np.array(['FB','HB','QB','RB','TE','WR'])
 
-# get intended receiver and defender
-receiver_name, defender_name = get_intended_receiver_and_defender(play_df, offense_positions, defense_positions)
-print(receiver_name)
-print(defender_name)
+## get intended receiver and defender
+#receiver_name, defender_name = get_intended_receiver_and_defender(play_df, offense_positions, defense_positions)
+#print(receiver_name)
+#print(defender_name)
 
-# plot play
-plot_play(play_df, offense_positions, defense_positions, receiver_name, defender_name)
+## plot play
+#plot_play(play_df, offense_positions, defense_positions, receiver_name, defender_name)
+
+home_query = 'playId == {:.0f} and team == "home"'.format(play_id_list[play_idx])
+away_query = 'playId == {:.0f} and team == "away"'.format(play_id_list[play_idx])
+football_query = 'playId == {:.0f} and displayName == "Football"'.format(play_id_list[play_idx])
+play_home = week_df.query(home_query)
+play_away = week_df.query(away_query)
+play_football = week_df.query(football_query)
+fig, ax = create_football_field(highlight_line=True, highlight_line_number=play_football.iloc[0]["x"]-10)
+plt.ion()
+plt.show()
+for i in range(1,len(play_football)+1):
+    frame_query = 'frameId == {:.0f}'.format(i)
+    home_pts = play_home.query(frame_query).plot(x='x', y='y', kind='scatter', ax=ax, color='orange', s=30, legend='Away')
+    away_pts = play_away.query(frame_query).plot(x='x', y='y', kind='scatter', ax=ax, color='blue', s=30, legend='Home')
+    football_pt = play_football.query(frame_query).plot(x='x', y='y', kind='scatter', ax=ax, color='black', s=30, legend='Home')
+    plt.pause(0.001)
--- util/visualization.py
+++ util/visualization.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import matplotlib.patches as patches
+
 
 # plot play
 def plot_play(play_df, offense_positions, defense_positions, receiver_name, defender_name):
@@ -24,3 +26,80 @@ def plot_play(play_df, offense_positions, defense_positions, receiver_name, defe
             ax.plot(x, y, color='b', linewidth=2)
     print(np.unique(play_df["event"][play_df["event"] != "None"])) # print events of play (not in chronological order)
     plt.show()
+
+
+# plot football field
+def create_football_field(linenumbers=True,
+                          endzones=True,
+                          highlight_line=False,
+                          highlight_line_number=50,
+                          highlighted_name='Line of Scrimmage',
+                          fifty_is_los=False,
+                          figsize=(12, 6.33)):
+    """
+    Function that plots the football field for viewing plays.
+    Allows for showing or hiding endzones.
+    """
+    rect = patches.Rectangle((0, 0), 120, 53.3, linewidth=0.1,
+                             edgecolor='r', facecolor='darkgreen', zorder=0)
+
+    fig, ax = plt.subplots(1, figsize=figsize)
+    ax.add_patch(rect)
+
+    plt.plot([10, 10, 10, 20, 20, 30, 30, 40, 40, 50, 50, 60, 60, 70, 70, 80,
+              80, 90, 90, 100, 100, 110, 110, 120, 0, 0, 120, 120],
+             [0, 0, 53.3, 53.3, 0, 0, 53.3, 53.3, 0, 0, 53.3, 53.3, 0, 0, 53.3,
+              53.3, 0, 0, 53.3, 53.3, 0, 0, 53.3, 53.3, 53.3, 0, 0, 53.3],
+             color='white')
+    if fifty_is_los:
+        plt.plot([60, 60], [0, 53.3], color='gold')
+        plt.text(62, 50, '<- Player Yardline at Snap', color='gold')
+    # Endzones
+    if endzones:
+        ez1 = patches.Rectangle((0, 0), 10, 53.3,
+                                linewidth=0.1,
+                                edgecolor='r',
+                                facecolor='blue',
+                                alpha=0.2,
+                                zorder=0)
+        ez2 = patches.Rectangle((110, 0), 120, 53.3,
+                                linewidth=0.1,
+                                edgecolor='r',
+                                facecolor='blue',
+                                alpha=0.2,
+                                zorder=0)
+        ax.add_patch(ez1)
+        ax.add_patch(ez2)
+    plt.xlim(0, 120)
+    plt.ylim(-5, 58.3)
+    plt.axis('off')
+    if linenumbers:
+        for x in range(20, 110, 10):
+            numb = x
+            if x > 50:
+                numb = 120 - x
+            plt.text(x, 5, str(numb - 10),
+                     horizontalalignment='center',
+                     fontsize=20,  # fontname='Arial',
+                     color='white')
+            plt.text(x - 0.95, 53.3 - 5, str(numb - 10),
+                     horizontalalignment='center',
+                     fontsize=20,  # fontname='Arial',
+                     color='white', rotation=180)
+    if endzones:
+        hash_range = range(11, 110)
+    else:
+        hash_range = range(1, 120)
+
+    for x in hash_range:
+        ax.plot([x, x], [0.4, 0.7], color='white')
+        ax.plot([x, x], [53.0, 52.5], color='white')
+        ax.plot([x, x], [22.91, 23.57], color='white')
+        ax.plot([x, x], [29.73, 30.39], color='white')
+
+    if highlight_line:
+        hl = highlight_line_number + 10
+        plt.plot([hl, hl], [0, 53.3], color='blue')
+        #plt.text(hl + 2, 50, '<- {}'.format(highlighted_name),
+        #         color='yellow')
+    return fig, ax
